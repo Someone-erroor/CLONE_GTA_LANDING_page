8398a6a0cb1353660c44055f5d3e4b6347df59c5
Ravi <ravikishan76768@gmail.com>
2026-07-14 12:15:45 +0530

    Initial commit

---
+++ js.js
@@ -0,0 +1,8 @@
+function showsidebar(){
+    const side = document.querySelector('.sidebar');
+    side.style.display= 'block';
+}
+function closebar(){
+        const side = document.querySelector('.sidebar');
+    side.style.display= 'none';   
+}
